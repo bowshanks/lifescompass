4914d037394af9a55b1b040fff80327994086c9b
Daniel Gardner <bowshanks@gmail.com>
2017-05-15 21:52:09 -0600

    career page

--- js/controllers/careerCtrl.js
+++ js/controllers/careerCtrl.js
@@ -1,3 +1,5 @@
-angular.module('lifescompass').controller('careerCtrl',function($scope){
-
+angular.module('lifescompass').controller('careerCtrl',function($scope, $location){
+  $scope.scrollTo = function (anchor) {
+    $location.hash(anchor);
+  }
 });
