d24815d87cd7007b08cb6e5806b178af8eaa6abe
Daniel Gardner <bowshanks@gmail.com>
2017-06-02 18:39:21 -0600

    parallax on quotes

--- js/directives/quoteDir.js
+++ js/directives/quoteDir.js
@@ -6,7 +6,18 @@ angular.module('lifescompass').directive('quoteDir', function($window) {
     },
     templateUrl: './js/directives/templates/quote.html',
     link: function(scope, element, attrs) {
-
+      scope.scroll = 0;
+      scope.height = $window.innerHeight - 100;
+      var transparancy = 0.8
+      scope.slowScroll = 'translate(0px,'+ scope.scroll /10 +'%)';
+      var windowEl = angular.element($window);
+      var handler = function() {
+        scope.scroll = $window.pageYOffset;
+        transparancy = (scope.scroll/scope.height) + 0.7;
+        scope.slowScroll = 'translate(0px,'+ scope.scroll /10 +'%)';
+      }
+      windowEl.on('scroll', scope.$apply.bind(scope, handler));
+      handler();
     }
   };
 });
