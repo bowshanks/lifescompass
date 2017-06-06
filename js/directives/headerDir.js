c774433a4da3d8adf0399d6d62dbd97a94cd043f
Daniel Gardner <bowshanks@gmail.com>
2017-06-05 19:26:15 -0600

    header hiding

--- js/directives/headerDir.js
+++ js/directives/headerDir.js
@@ -3,14 +3,16 @@ angular.module('lifescompass').directive('headerDir', function($window) {
     templateUrl: './js/directives/templates/header.html',
     link: function(scope, element, attrs) {
       scope.scroll = 0;
-      scope.height = $window.innerHeight - 100;
-      var transparancy = 0.8
-      scope.bgCol = 'rgba(255,255,255, '+ transparancy +')';
       var windowEl = angular.element($window);
+      scope.headerDisplay = true;
       var handler = function() {
         scope.scroll = $window.pageYOffset;
-        transparancy = (scope.scroll/scope.height) + 0.7;
-        scope.bgCol = 'rgba(255,255,255, '+ transparancy +')';
+        if (scope.scroll > 200) {
+          scope.headerDisplay = false;
+        }
+        if (scope.scroll < 200) {
+          scope.headerDisplay = true;
+        }
       }
       windowEl.on('scroll', scope.$apply.bind(scope, handler));
       handler();
