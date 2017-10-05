9dcc4e33d749cba7c798598bb77eeac71d58f1bf
Daniel Gardner <bowshanks@gmail.com>
2017-10-04 18:48:21 -0600

    moslty flow changes

--- js/directives/quoteDir.js
+++ js/directives/quoteDir.js
@@ -8,13 +8,13 @@ angular.module('lifescompass').directive('quoteDir', function($window) {
     link: function(scope, element, attrs) {
       scope.scroll = 0;
       scope.height = $window.innerHeight - 100;
-      var transparancy = 0.8
       scope.slowScroll = 'translate(0px,'+ scope.scroll /10 +'%)';
       var windowEl = angular.element($window);
       var handler = function() {
+        var elHeight = document.getElementById("quote").offsetHeight;
+        scrollPercent = elHeight / 20;
         scope.scroll = $window.pageYOffset;
-        transparancy = (scope.scroll/scope.height) + 0.7;
-        scope.slowScroll = 'translate(0px,'+ scope.scroll /5 +'%)';
+        scope.slowScroll = 'translate(0px,'+ scope.scroll / scrollPercent +'%)';
       }
       windowEl.on('scroll', scope.$apply.bind(scope, handler));
       handler();
